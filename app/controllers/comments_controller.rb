736e841dd77997e1b600f2b38c0a7347961c8dd6
rodic <alec.rodic@gmail.com>
2015-02-19 14:28:01 +0100

    Refactored comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    @comment.post_id = params[:post_id]
+    @comment = comment_from_params
     if @comment.save
       redirect_to post_path(params[:post_id]), notice: "Comment added successfully!"
     else
@@ -15,8 +13,12 @@ class CommentsController < ApplicationController
 
   private
   
-    def comment_params
-      params.require(:comment).permit(:content)
+    def comment_from_params
+      Comment.new do |c|
+        c.content = params[:comment][:content]
+        c.user_id = current_user.id
+        c.post_id = params[:post_id]
+      end
     end
 
 end
