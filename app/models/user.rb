1998af35971e3adf5522c88b9eba3c127206d2ce
rodic <alec.rodic@gmail.com>
2015-02-21 12:09:32 +0100

    Google signin code refactored

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,18 +64,16 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    user = User.find_by(provider: access_token.provider, uid: access_token.uid)
     if user
       return user
     else
-      registered_user = User.where(:email => access_token.info.email).first
+      registered_user = User.find_by_email(access_token.info.email)
       if registered_user
         return registered_user
       else
-        user = User.create(provider: access_token.provider,
-                           email: data["email"],
-                           uid: access_token.uid ,
-                           password: Devise.friendly_token[0,20])
+        User.create!(provider: access_token.provider, email: data["email"], uid: access_token.uid ,
+                     password: Devise.friendly_token[0,20])
       end
     end
   end
