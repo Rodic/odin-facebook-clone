44f96aee19fb2ce9c34cc61f9862cdc247195560
rodic <alec.rodic@gmail.com>
2015-02-21 15:54:31 +0100

    Setting production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'https://odin-facebook.herokuapp.com/' }
+  
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = 
+  {
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => ENV['EMAIL_ADDRESS']
+    :password           => ENV['EMAIL_PASSWORD']
+  }
 end
