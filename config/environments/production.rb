7863410fb82686f30450e68ba8bb2ec8086cac1f
rodic <alec.rodic@gmail.com>
2015-02-21 15:56:29 +0100

    Syntax error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     :port               => 587,
     :domain             => 'gmail.com',
     :authentication     => :plain,
-    :user_name          => ENV['EMAIL_ADDRESS']
-    :password           => ENV['EMAIL_PASSWORD']
+    :user_name          => ENV['EMAIL_ADDRESS'],
+    :password           => ENV['EMAIL_PASSWORD'],
   }
 end
